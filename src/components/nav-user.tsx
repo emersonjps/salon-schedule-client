358fc9f15bbcd41df86dd26b2d32eb8c5c35c07a
emersonjps <emerson20010213@gmail.com>
2025-03-06 17:03:00 -0300

    feat: atualiza o componente NavUser para incluir ícone de usuário e altera URL do avatar no Sidebar

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -7,6 +7,7 @@ import {
   CreditCard,
   LogOut,
   Sparkles,
+  UserRound,
 } from "lucide-react"
 
 import {
@@ -75,7 +76,9 @@ export function NavUser({
             >
               <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                <AvatarFallback className="rounded-lg">
+                  <UserRound />
+                </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{user.name}</span>
